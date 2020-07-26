5733d86c83d443fc04bc42379297b058a170b728
szkatalin <szkatalin@outlook.com>
2020-07-26 18:36:01 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import * as bodyParser from 'body-parser';
-// import { BadRequestException } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
+import { BadRequestException } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.use(
-  //   bodyParser.json({
-  //     type: (req: any) => {
-  //       if (req.get('Content-Type') === 'application/json') {
-  //         return req.get('Content-Type') === 'application/json';
-  //       }
-  //       throw new BadRequestException();
-  //     },
-  //     strict: false
-  //   })
-  // );
+  app.use(
+    bodyParser.json({
+      type: (req: any) => {
+        if (req.get('Content-Type') === 'application/json') {
+          return req.get('Content-Type') === 'application/json';
+        }
+        throw new BadRequestException();
+      },
+      strict: false
+    })
+  );
 
   const port = process.env.PORT || '3000';
   await app.listen(port);
