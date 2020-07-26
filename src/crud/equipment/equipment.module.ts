dc3b7c00d0d0a361d9327a594363d5fe2f9d0eaf
szkatalin <szkatalin@outlook.com>
2020-07-26 18:54:33 +0200

    update

--- src/crud/equipment/equipment.module.ts
+++ src/crud/equipment/equipment.module.ts
@@ -4,16 +4,9 @@ import { EquipmentController } from './equipment.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Equipment } from './model/equipment.entity';
 import { EquipmentRepository } from './equipment.repository';
-import { LocationRepository } from '../location/location.repository';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      Equipment,
-      EquipmentRepository,
-      LocationRepository
-    ])
-  ],
+  imports: [TypeOrmModule.forFeature([Equipment, EquipmentRepository])],
   providers: [EquipmentService],
   controllers: [EquipmentController]
 })
