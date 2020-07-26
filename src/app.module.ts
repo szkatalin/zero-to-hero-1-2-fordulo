fd4f1b30b4030fb7fc098af9ecf3ae68c3a7e7a8
szkatalin <szkatalin@outlook.com>
2020-07-26 18:27:55 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CrudModule } from './crud/crud.module';
 import { HttpStatusModule } from './http-status/http-status.module';
 import { SquaresModule } from './squares/squares.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [CrudModule, HttpStatusModule, SquaresModule]
+  imports: [CrudModule, HttpStatusModule, SquaresModule],
+  controllers: [AppController]
 })
 export class AppModule {}
